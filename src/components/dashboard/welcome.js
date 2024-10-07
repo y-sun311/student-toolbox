673c6dd6f96959c3add804c2347bc5e5278d5efe
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-10-07 18:24:09 +1300

    Feat homepage responsive (#96)
    * frontend:Reduce top margin on dashboard for smaller screens to improve responsiveness.
    
    * frontend:adjusted welcome text size for tablet and mobile screens.

--- src/components/dashboard/welcome.js
+++ src/components/dashboard/welcome.js
@@ -1,11 +1,14 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+
+import "./styles/welcome.css";
 import Title from "../title/title";
 
 export default function Welcome() {
   const session = useSession();
   const username = session?.data?.user?.name;
 
-  return <Title text={`👋 Welcome, ${username || "student"}!`} />;
+  return <div className="welcome-text"><Title text={`👋 Welcome, ${username || "student"}!`}  /></div>;
+
 }
