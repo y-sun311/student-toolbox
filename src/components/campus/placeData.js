5ba1b3241696354c6aae5acf7f18fd2856b7b053
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-10-08 03:21:01 +1300

    added location data.

---
+++ src/components/campus/placeData.js
@@ -0,0 +1,28 @@
+// Define and export the list of places
+export const placesData = [
+    {
+        label: "General Library",
+        coords: { lat: -36.8526870, lng: 174.7695090},
+        type: "Building",
+    },
+    {
+        label: "Faulty of Engineering",
+        coords: { lat: -36.8526870, lng: 174.7698560 },
+        type: "Building",
+    },
+    {
+        label: "OGGB",
+        coords: { lat: -36.8531994, lng: 174.7710221 },
+        type: "Building",
+    },
+    {
+        label: "Carlaw Park Student Village",
+        coords: { lat: -36.8523503, lng: 174.7742499 },
+        type: "Accommodation",
+    },
+    {
+        label: "Grafton Hall",
+        coords: { lat: -36.8620880, lng: 174.7666622 },
+        type: "Accommodation",
+    },
+];
