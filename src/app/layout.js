3993770d004412b54239fa91e3e99a7f6bbf2bea
Hayley Smith <126207971+hayley9080@users.noreply.github.com>
2024-10-09 08:07:57 +1300

    fixed issue with invalid DOM property (#98)
    * fixed issue with invalid DOM property
    
    * fix cross origin case

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
       <body>
         <NextAuthProvider>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
